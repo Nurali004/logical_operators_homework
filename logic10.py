c5638c3ee9772ee3ff9f114173526ce2081548e2
M A C W I N <mavzurovnurali@gmail.com>
2025-04-29 01:22:57 +0500

    1110

--- logic10.py
+++ logic10.py
@@ -1,9 +1,12 @@
 def main(a):
     """
-    Given integer a,  check the following statement "The integer is two-digit number".
+    Given integer a,  check the following statement "The integer is two-digit numb
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+    return 10 <= abs(a) <=99
+
+a=int(input("sonni kiriting:"))
+print(main(a))
